1752667ba4aa401d0b78c57dce7c91daa6bd1e59
fatimabatool <fatima.batool@xloopdigital.com>
2023-04-16 11:48:26 -0700

    test 1 failed

--- src/Cart.js
+++ src/Cart.js
@@ -1,9 +1,4 @@
-const Item = require('../src/Item.js');
-
 module.exports = class Cart {
- constructor(){
-    this.items = [];
-    this.totalPrice = 0;
+  
 }
 
-}
--- test/Cart.test.js
+++ test/Cart.test.js
@@ -2,10 +2,12 @@ const Cart = require('../src/Cart.js');
 const expect = require('chai').expect;
 
 describe('Cart', () => {
-    it('should initialize as empty', () => {
-        const cart = new Cart()
+  test ('should initialize as empty', () => {
+      // Set
+      const cart = new Cart()
 
-        expect(cart.items).to.deep.equal([])
-        expect(cart.totalPrice).to.be.equal(0)
-    })
+      // Execute and Assert
+      expect(cart.items).to.deep.equal([])
+      expect(cart.totalPrice).to.be.equal(0)
+  })
 })
