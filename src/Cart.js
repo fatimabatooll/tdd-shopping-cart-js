3e8e9a2d5a9479e6e6f41aee870026f1a4db58fe
fatimabatool <fatima.batool@xloopdigital.com>
2023-04-16 14:06:11 -0700

    test 2 passed

--- src/Cart.js
+++ src/Cart.js
@@ -4,7 +4,22 @@ class Cart {
     this.items = [];
   }
 
-  
+  addItem(item, quantity) {
+    const existingItemIndex = this.items.findIndex(
+      (cartItem) => cartItem.item === item
+    );
+
+    if (existingItemIndex !== -1) {
+      this.items[existingItemIndex].quantity += quantity;
+    } else {
+      this.items.push({ item, quantity });
+    }
+
+    this.totalPrice += item.price * quantity;
+  }
+
+
+
 }
 
 module.exports = { Cart };
